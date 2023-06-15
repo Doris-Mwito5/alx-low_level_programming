f5b84c7e6649bd4dc05334243d57627962d153f9
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-15 12:13:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -main function of the program
+ *
+ * Return: returns zero when the program executes successfully
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}

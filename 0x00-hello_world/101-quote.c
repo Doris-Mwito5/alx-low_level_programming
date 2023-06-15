1c32aec629be774ff72f14d88bbd31214b90a0b8
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-15 13:31:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main -main function of the program
- *
- * Return: always 1
+* main - main function of the program
+*
+* Return: returns always 1
 */
 
 int main(void)
 
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19, 59");
 
-return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+return (1);
 }

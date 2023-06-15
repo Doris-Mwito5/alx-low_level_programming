81717ecafc24ab40de940a5508725927df545d46
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-15 13:04:44 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -main function of the program
+ *
+ * Return: always 1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19);
+return (1);
+}

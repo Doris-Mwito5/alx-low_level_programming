03e365f289590d51ef6e202e94225ae15512f321
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-16 03:08:44 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - main function of the program
+ *program on C variables
+ *
+ * Return: returns zero when the program executes successfully
+ */
+int main(void)
+{
+	int n;
+if (n < 0)
+{
+	printf("is negative");
+}
+else if (n > 0)
+{
+	printf("is positive");
+}
+else
+{
+	printf("is zero");
+}
+
+return (0);
+}

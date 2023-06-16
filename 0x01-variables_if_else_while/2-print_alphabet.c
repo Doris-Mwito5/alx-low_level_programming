8bf89475a570633b46f4ebc1232eff010600d20e
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-16 04:44:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - main function of the program
+ *program on C variables
+ *
+ * Return: returns zero when the program executes successfully
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'A', x <= 'Z'; x++)
+{
+	putchar("%c", x);
+}
+return (0);
+}

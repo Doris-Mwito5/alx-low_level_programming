25d8516bd0708989a70a1af0bf5c57d9f89184a1
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-16 08:13:54 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - main function of the program
+ *
+ * Return: returns 0 when the program executes successfully
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = '0'; m < '9'; m++)
+	{
+		for (n = m + 1; n <= '9'; n++)
+		{
+			if (n != m)
+			{
+				putchar(m);
+				putchar(n);
+				if (m == '8' && p == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

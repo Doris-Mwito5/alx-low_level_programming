f6f57043dd8cc6ed8c9ece4f7f738f92f4732992
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-16 07:26:13 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - main function of the program
+ *
+ *Return: returns 0 when the program executes successfully
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}

6e7de427335a09edaffc6ee18074481a1232f66f
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-20 05:26:06 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - main function
+ *@c: character to be checked
+ * Return: returns 1 for lowercase amd zero for anything else
+ */
+int _islower(int c)
+{
+	if (c >= 97; &&c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}

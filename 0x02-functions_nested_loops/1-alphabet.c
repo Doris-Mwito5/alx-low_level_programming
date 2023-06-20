2258163d5427c6df1864e5a9e5326e53cdad3faa
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-20 03:50:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function of the program
+ *
+ * Return: returns 0 when the program executes successfully
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar(x);
+	return (0);
+}

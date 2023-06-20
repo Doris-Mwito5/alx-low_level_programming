d18f77918d1f6e76a156e4a5116ac3215d5ae7f9
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-20 09:23:14 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.c"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: returns 0
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

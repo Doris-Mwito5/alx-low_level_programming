846d2fffeae197f3e07f17ab6cc3e6217b3f669d
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-20 10:05:47 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n < 98)
-			_putchar("%d", n--);
-		_putchar("%d\n", n);
+			printf("%d", n--);
+		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-			_putchar("%d", n++);
-		_putchar("%d\n", n);
+			printf("%d", n++);
+		printf("%d\n", n);
 	}
 }

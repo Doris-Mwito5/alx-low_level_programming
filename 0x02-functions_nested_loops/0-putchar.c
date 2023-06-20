dd4a2aa002ab6655fb3d78ad605febd42fb6dbdd
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-20 02:31:37 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function of the program
+ *
+ * Return: returns 0 when the prograam executes successfully
+ */
+int main(void)
+{
+	char myfun[] = "_putchar";
+
+	_putchar("%s", myfun);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C functions and nested loops

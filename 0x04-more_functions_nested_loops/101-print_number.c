6aa97c5f1f2da795929fbf03cd76c091c92c30f2
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-22 16:42:06 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,17 +7,21 @@
  */
 void print_number(int n);
 {
-	unsigned int a = n;
+	unsigned int a;
 
 	if (n < 0)
 	{
-		n *= -1;
-		a = n;
+		a = -n;
 		_putchar('-');
 	}
-	a /= 10;
 
-	if (a != 0)
-		print_number(a);
-	_putchar((unsigned int) n % 10 + '0');
+	else
+	{
+		a = n;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
 }

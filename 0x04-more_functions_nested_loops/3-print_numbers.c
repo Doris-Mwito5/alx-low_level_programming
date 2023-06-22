40371496ced4accec3e70fd1b48bffb4386bba41
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-22 12:25:21 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - outputs numbers 0-9
+* Return: returns success (0)
+*/
+void print_numbers(void)
+{
+	char a;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

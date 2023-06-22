e2dd2196353f9bf7d8bc53af7c6921f996cd7145
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-22 12:12:30 -0700

    files added

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef main_h
+#define main_h
+
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,3 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif

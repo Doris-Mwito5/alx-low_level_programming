a114aba677424d22c5fa9ad6d36b2ac30dc1131e
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-22 14:26:00 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ *@n: number of lines
+ * Return: returns a straight line
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}

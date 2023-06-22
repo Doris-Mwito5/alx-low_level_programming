c08c0f851ff71abba07b53e019105f67e891b861
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-22 10:44:15 -0700

    files added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function of the program
+ *
+ * Return: returns 0 on successful execution
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+        positive_or_negative(i);
+	return (0);
+}

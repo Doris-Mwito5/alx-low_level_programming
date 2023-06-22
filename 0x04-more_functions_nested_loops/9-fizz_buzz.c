9c1601ec18cc9c62ff9f0e212ebe16d22e76aa2c
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-22 16:12:51 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,21 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0 && a % 5 != 0)
-		{
-			printf("Fizz");
-		}
-		else if (a % 5 == 0 && a % 3 != 0)
+		if (a % 5 = 0)
 		{
 			printf("Buzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else if (a % 3 == 0 && a % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i == 1)
+		else if (a != 100)
 		{
-			printf("%d", a);
+			printf(" ");
 		}
 		else
 		{

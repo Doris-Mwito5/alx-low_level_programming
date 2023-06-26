e2309cee7faf221bcd3d6b84141cb91326c26061
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-26 09:40:15 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,12 @@ void print_array(int *a, int n)
 
 	for (x = 0; x < n; x++)
 	{
+		printf("%d", a[x]);
+
 		if (x == (n - 1))
 		{
 			printf(", ");
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }

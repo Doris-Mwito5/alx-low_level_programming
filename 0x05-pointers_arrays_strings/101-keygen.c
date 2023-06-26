8db8f1636337b68cb3ec73bf0a6adc62175db70b
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-26 11:21:42 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0 on succesful execution
+ */
+int main(void)
+{
+	int x[100];
+	int a, b, c;
+
+	c = 0;
+
+	srand(time(NULL));
+
+	for (a = 0; a < 100; a++)
+
+	{
+		pass[a] = rand() % 78;
+
+		c += (pass[a] + '0');
+		putchar(pass[a] + '0');
+		if ((2772 - c) - '0' < 78)
+		{
+			b = 2772 - c - '0';
+			c += b;
+			putchar(b + '0');
+			break;
+		}
+	}
+	return (0);
+}

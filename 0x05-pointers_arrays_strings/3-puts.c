bd3ca9515731c2a124d2d79a222b3fc762987237
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-26 08:19:03 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the character value
+ * Return: returns 0 on successful execution
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}

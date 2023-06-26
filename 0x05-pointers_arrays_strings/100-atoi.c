f400d6c109992d5ef32f4dca0d0d51ebcbaebdd2
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-26 10:59:53 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,41 +8,41 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int a, b, c, d, e, f;
 
-	i = 0;
+	a = 0;
+	b = 0;
+	c = 0;
 	d = 0;
-	n = 0;
-	len = 0;
+	e = 0;
 	f = 0;
-	digit = 0;
 
-	while (s[len] != '\0')
-		len++;
+	while (s[d] != '\0')
+		d++;
 
-	while (i < len && f == 0)
+	while (a < d && f == 0)
 	{
-		if (s[i] == '-')
-			++d;
+		if (s[a] == '-')
+			++b;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[a] >= '0' && s[a] <= '9')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			f = s[a] - '0';
+			if (b % 2)
+				f = -f;
+			c = c * 10 + f;
+			e = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
 				break;
-			f = 0;
+			e = 0;
 		}
-		i++;
+		a++;
 	}
 
-	if (f == 0)
+	if (e == 0)
 		return (0);
 
-	return (n);
+	return (c);
 }
 
 

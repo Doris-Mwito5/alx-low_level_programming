0c95853dff7d2070ef6fb2a627440c83a493e5ec
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-26 09:26:19 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,11 @@ void puts2(char *str)
 {
 	int x;
 
-	for (x = 0; s[x] != '\0'; x++)
+	for (x = 0; str[x] != '\0'; x++)
 	{
 		if (x % 2 == 0)
 		{
-			_putchar([x]);
+			_putchar(str[x]);
 		}
 	}
 	_putchar('\n');

7cc7271909e2ac54f51c6a99c86517eb8e464aaa
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-29 01:17:59 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array -  Reverses the content of an array of integers
+ * @n: Array elements
+ * @a: character value
+ * Return: returns 0 on successful execution
+ */
+void reverse_array(int *a, int n)
+{
+	int a;
+
+	a = 0;
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
+}

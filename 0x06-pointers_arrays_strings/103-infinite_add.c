de11089afddfc7e818e2a492028a5ca71474d525
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-29 02:50:37 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,42 +9,42 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int res = 0, a = 0, b = 0, ch = 0;
-	int res2 = 0, res3 = 0, ch2 = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + a) != '\0')
-		a++;
-	while (*(n2 + b) != '\0')
-		b++;
-	a--;
-	b--;
-	if (b >= size_r || b >= size_r)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	while (b >= 0 || a >= 0 || res == 1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (a < 0)
-			res2 = 0;
+		if (i < 0)
+			val1 = 0;
 		else
-			res2 = *(n1 + a) - '0';
-		if (b < 0)
-			res3 = 0;
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
 		else
-			res3 = *(n2 + b) - '0';
-		ch2 = res2 + res3 + res;
-		if (ch2 >= 10)
-			res = 1;
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
 		else
-			res = 0;
-		if (ch >= (size_r - 1))
+			overflow = 0;
+		if (digits >= (size_r - 1))
 			return (0);
-		*(r + ch) = (ch2 % 10) + '0';
-		ch++;
-		b--;
-		a--;
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (ch == size_r)
+	if (digits == size_r)
 		return (0);
-	*(r + ch) = '\0';
+	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
 }

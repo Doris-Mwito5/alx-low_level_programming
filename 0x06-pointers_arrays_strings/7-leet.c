672153375446943ef117935a7839db2362f4ecf7
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-06-29 02:13:24 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,24 @@
 #include "main.h"
 /**
  * leet - encode a string into 1337
- * @s: The string
+ * @n: The string
  * Return: returns 0 on successful execution
  */
-char *leet(char *s)
+char *leet(char *n)
 {
-	int a;
+	int a, b;
+	char sh[] = "aAeEoOtTlL";
+	char ch[] = "4433007711";
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; n[a] != '\0'; a++)
 	{
-		while (s[a] == 'a' || s[a] == 'A')
-			s[a] = '4';
-		while (s[a] == 'e' || s[a] == 'E')
-			s[a] = '3';
-		while (s[a] == 'o' || s[a] == 'O')
-			s[a] = '0';
-		while (s[a] == 't' || s[a] == 'T')
-			s[a] = '7';
-		while (s[a] == 'l' || s[a] == 'L')
-			s[a] = '1';
+		for (b = 0; b < 10; b++)
+		{
+			if (n[a] == sh[b])
+			{
+				n[a] = ch[b];
+			}
+		}
 	}
-
-	return (s);
+	return (n);
 }

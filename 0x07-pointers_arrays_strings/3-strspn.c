cfb3486487e9976b3965ee19beb76e654c8d7a9a
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-07-03 05:20:18 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: value1
+ * @accept: value2
+ * Return: returns 0 on successful execution
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0;
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+			{
+				a++;
+				break;
+			}
+			else if (accept[b + 1] == '\0')
+				return (a);
+		}
+		s++;
+	}
+	return (a);
+}

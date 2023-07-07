009726853e4ce9ec6daece7955d70bbc15a962b7
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-07-07 06:06:48 -0700

    libmy.a

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Return: returns 0 on succesful execution
+ */
+int main(void)
+{
+	_puts("\"My goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}

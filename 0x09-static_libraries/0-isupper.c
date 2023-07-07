e3c912aaa698865192029ba746d49db3276f46fb
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-07-07 04:55:23 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - prints uppercase letters
+ * @c: char
+ * Return: returns 0 on succesful execution
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+
+		return (1);
+	}
+
+	else
+	return (0);
+}

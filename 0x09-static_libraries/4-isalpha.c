346c53a2c4b4b38f81b87b5e0d61817458e5fd1a
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-07-07 05:55:50 -0700

    4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ *  _isalpha - checks if its a character
+ *  @c:character to be checked
+ *  Return: returns 1 if the character is an alphabet and 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 65 && 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }

6caff9683dbc8bd68861c09159694518d44db971
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-07-10 01:53:27 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - free up grid allocated by alloc_grid
+ * @grid: - grid to free
+ * @height: - height of grid
+ * Return: returns a 0 on succesful execution
+ */
+void free_grid(int **grid, int height)
+{
+	if (!grid)
+		return;
+	if (!grid[0])
+		return;
+	while (height-- > 0)
+		free(grid[height]);
+	free(grid);
+}

9fb2d57f26266dc71f5c75645f397c54bf61a6a2
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-07-20 07:49:49 -0700

    added files

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function of the program
+ *
+ * Return: returns 0 when the program executes succcessfully
+ */
+int main(void)
+{
+	char myfun[] = "_putchar";
+
+	_putchar("%s",myfun[]);
+	return (0);
+}

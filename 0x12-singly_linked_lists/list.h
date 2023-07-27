306361a13f3e1fa5a0a4ad892b89013fae9b0e56
kawiramwito3@gmail.com <kawiramwito3@gmail.com>
2023-07-27 12:08:20 -0700

    update

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -2,16 +2,18 @@
 #define LISTS_H
 
 /**
- * struct list_s - the singly linked list.
- * @str: the string 
- * @len: string length
- * @next: poiner to the next node.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);

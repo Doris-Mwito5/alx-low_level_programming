066144aa7b65db828dbc21e38b51441b4bcfa7b2
kawiramwito3@gmail.com <kawiramwito3@gmail.com>
2023-07-27 13:14:05 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+4-free_list.c
+
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: the list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}

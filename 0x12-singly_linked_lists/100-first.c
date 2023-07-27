51c1e184e345fd69086a8762240a124586133dae
kawiramwito3@gmail.com <kawiramwito3@gmail.com>
2023-07-27 13:18:37 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,7 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - outputs a sentence
  */
 void first(void)
 {

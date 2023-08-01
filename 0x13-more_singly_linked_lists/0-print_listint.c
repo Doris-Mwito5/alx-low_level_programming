8c3f290b8fc2a82f5029435d368cf692d5011c42
kawiramwito3@gmail.com <kawiramwito3@gmail.com>
2023-08-01 10:26:37 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		i++;
+		num++;
 		h = h->next;
 	}
 
-	return (i);
+	return (num);
 }
